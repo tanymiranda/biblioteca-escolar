f15e4061ff29f6407be52523f3d66426b9079570
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2024-11-26 19:52:42 -0300

    Update supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fxqjorucrqeunnhxjavl.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4cWpvcnVjcnFldW5uaHhqYXZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyNzY3MTQsImV4cCI6MjA0Njg1MjcxNH0.dOyajCHTtIer10BqJn3TiMKK_iUvDNerduvb4lo3JDg'; 
+const supabaseUrl = 'sua url aqui'; 
+const supabaseKey = 'sua api key aqui'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
